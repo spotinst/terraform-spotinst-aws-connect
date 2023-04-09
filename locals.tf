7b2ebd81331add7b6bf0af37ea38534e50edca53
jnovick <novickjd@gmail.com>
2023-04-09 12:18:39 +0300

    Only require AWS account alias when name is unspecified

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   organization_id = data.external.account.result["organization_id"]
   cloudProvider   = data.external.account.result["cloud_provider"]
   spotinst_token  = var.debug == true ? nonsensitive(var.spotinst_token) : var.spotinst_token
-  name            = var.name == null ? data.aws_iam_account_alias.current.account_alias : var.name
+  name            = var.name == null ? data.aws_iam_account_alias.current.0.account_alias : var.name
   random          = random_id.random_string.hex
 }
 
